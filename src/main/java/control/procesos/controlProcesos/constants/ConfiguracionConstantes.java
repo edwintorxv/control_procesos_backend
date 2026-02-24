bf15dcb9e2f01b42c5af81ffc73a20b6ad4a6313
EDWIN GUSTAVO GACHA AMADO <gachae@HILDEBOG.LOCAL>
2026-02-24 11:44:01 -0500

    Ajuste cors

---
+++ src/main/java/control/procesos/controlProcesos/constants/ConfiguracionConstantes.java
@@ -0,0 +1,9 @@
+package control.procesos.controlProcesos.constants;
+
+public class ConfiguracionConstantes {
+
+    private ConfiguracionConstantes(){}
+
+    public static final String ACCESO_CORS = "http://localhost:4200";
+
+}
